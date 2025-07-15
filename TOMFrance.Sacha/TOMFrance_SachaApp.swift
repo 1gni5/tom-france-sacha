4651939f2d8b84eb22980b9b9cd1081e2f68c9e4
1gni5 <jules.ducange@posteo.net>
2025-07-15 14:04:15 +0200

    Initial Commit

---
+++ TOMFrance.Sacha/TOMFrance_SachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOMFrance_SachaApp.swift
+//  TOMFrance.Sacha
+//
+//  Created by Jules Ducange on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TOMFrance_SachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TOMFrance.SachaTests/TOMFrance_SachaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TOMFrance_SachaTests.swift
+//  TOMFrance.SachaTests
+//
+//  Created by Jules Ducange on 15/07/2025.
+//
+
+import Testing
+@testable import TOMFrance_Sacha
+
+struct TOMFrance_SachaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
