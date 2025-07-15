5c70b25fd5144aa4fd9043c3aabb442d20872df9
LeCaribooo <pierrealexis.valbrun@doctolib.com>
2025-07-15 14:56:38 +0200

    chore: add gitignore

---
+++ TOMFrance.Sacha/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  TOMFrance.Sacha
+//
+//  Created by Pierre Alexis Valbrun on 15/07/2025.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
