012ba67713f88abb7ae9fe3ddfefcd57aacf5914
1gni5 <jules.ducange@posteo.net>
2025-07-16 11:39:37 +0200

    removed stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-// import { CreateWordDialog } from './components/createWordDialog'
-// import { Button } from './components/ui/button'
-// import { WordTable } from './components/WordTable'
-
 import { MenuPage } from "./MenuPage";
 
-function isOffline() {
-  return !navigator.onLine;
-}
 
 function App() {
   return (
     <>
-      {/* <h1 className="text-2xl font-bold underline"> Hello World! </h1> */}
       <MenuPage></MenuPage>
-      <div className="flex justify-between">
-        <p>hello updated !</p>
-        <p className="text-red-500">{isOffline() ? "Offline" : "Online"}</p>
-      </div>
 
     </>
   )
